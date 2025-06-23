540652ad2aca5079ef0cc43dd213f359a7bee09a
Logeskumar18 <kumarloges329@gmail.com>
2025-06-23 18:09:33 +0530

    Added

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import cors from 'cors'
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const port = 3000 || process.env.PORT;
+const MONGO_URL = 'mongodb+srv://loges:loges123@cluster0.lbatjkt.mongodb.net/express'
+
+mongoose.connect(MONGO_URL).then(()=>{
+    console.log('Databace Connected');
+    
+})
+
+
 
 const app = express();
 
