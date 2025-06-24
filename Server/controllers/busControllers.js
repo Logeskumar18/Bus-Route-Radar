680a03a12f23feab47e2bb3d7f6b03d95899d6f5
Logeskumar18 <kumarloges329@gmail.com>
2025-06-24 17:11:34 +0530

    Added

--- Server/controllers/busControllers.js
+++ Server/controllers/busControllers.js
@@ -1,13 +1,19 @@
 import BusRoute from "../model/busRouteModel.js";
 
 
-
 export const getAllBusRoutes = async (req, res) => {
   try {
     const busRoutes = await BusRoute.find();
-    res.json({ success: true, data: busRoutes });
+
+    if (busRoutes.length === 0) {
+      return res.status(404).json({ message: "No bus routes found" });
+    }
+
+    
+    res.status(200).json(busRoutes);
   } catch (error) {
     console.error("Error retrieving bus data:", error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
