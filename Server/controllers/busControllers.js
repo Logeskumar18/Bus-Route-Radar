040e1f28bc3c872aeb21f4d691c3c3d84f878681
Loges <kumarloges329@gmail.com>
2025-06-30 18:33:22 +0530

    Add Controller Functions

--- Server/controllers/busControllers.js
+++ Server/controllers/busControllers.js
@@ -1,24 +1,10 @@
 import BusRoute from "../model/busRouteModel.js";
 
 
-export const getAllBusRoutes = async (req, res) => {
-  try {
-    const busRoutes = await BusRoute.find({}, {_id:0,busNumber:1,from:1,to:1});
 
-    if (busRoutes.length === 0) {
-      return res.status(404).json({ message: "No bus routes found" });
-    }
-
-    
-    res.status(200).json(busRoutes);
-  } catch (error) {
-    console.error("Error retrieving bus data:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
 
 export const getBusesBetweenStops = async (req, res) => {
-  const { from, to } = req.query;
+  const { from, to } = req.body;
 
   if (!from || !to) {
     return res.status(400).json({ success: false, message: "'from' and 'to' are required query parameters." });
--- Server/routes/busRoutes.js
+++ Server/routes/busRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getAllBusRoutes, getBusDetailsByNumber, getBusesBetweenStops, getBusesByStopName } from "../controllers/busControllers.js"; 
+import { getBusDetailsByNumber, getBusesBetweenStops, getBusesByStopName } from "../controllers/busControllers.js"; 
 
 const busRouter = express.Router();
 
 
-busRouter.get("/busroutes", getAllBusRoutes);
-busRouter.get('/find-route', getBusesBetweenStops)
+
+busRouter.post('/find-route', getBusesBetweenStops)
 busRouter.post('/find-details', getBusDetailsByNumber)
 busRouter.post('/buses-by-stop', getBusesByStopName)
 
