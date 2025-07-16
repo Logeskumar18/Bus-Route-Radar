d9926d30118849c6c0c0d223078c108edcafc694
Logeskumar18 <kumarloges329@gmail.com>
2025-07-16 22:40:10 +0530

    Added

--- Client/src/pages/SearchByStop.jsx
+++ Client/src/pages/SearchByStop.jsx
@@ -20,7 +20,7 @@ const SearchByStop = () => {
     setBuses([]);
 
     try {
-      const res = await fetch('https://bus-route-sayw.onrender.com/stop', {
+      const res = await fetch('https://bus-route-sayw.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ stopName: stopName.trim() })
